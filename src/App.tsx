707a1887303923b0c54ca5fdc35596496028c197
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-10-11 16:35:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/common.scss';
 import Error404 from './components/Error404/Error404';
 import { Wrapper } from './components/Wrapper';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { Main } from './components/Main';
-import { LandingPage } from './components/LandingPage';
-import { Success } from './components/Success';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import { SelectionScreen } from './components/SelectionScreen';
-import { Loading } from './components/Loading';
 
-export const appURL = '/fe-dev-diplom';
-export const serverURL = 'https://fe-dev-diplom1.onrender.com';
+export const appURL = '/fe-diploma';
+export const serverURL = 'https://fe-diplom.herokuapp.com';
 
 function App(): ReactElement {
   return (
