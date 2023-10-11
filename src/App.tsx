b3c747591fb596eaa049f8ddc56ef9506bb39f3b
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-10-11 16:28:08 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/common.scss';
 import Error404 from './components/Error404/Error404';
 import { Wrapper } from './components/Wrapper';
@@ -18,23 +18,15 @@ export const serverURL = 'https://fe-dev-diplom1.onrender.com';
 function App(): ReactElement {
   return (
     <Router basename={appURL}>
-      <ScrollToTop />
-      <Wrapper>
-        <Header />
-        <Main>
-          <Router>
-            <Routes>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/select" component={SelectionScreen} />
-            <Route exact path="/success" component={Success} />
-            <Route exact path="/items/:id.html" component={Error404} />
-            <Route exact path="/loading" component={Loading} />
-            <Route path="*" component={Error404} /> CartIcon
-            </Routes>
-          </Router>
-        </Main>
-        <Footer />
-      </Wrapper>
+      <Switch>
+        <Route exact path="/" component={Wrapper} />
+        <Route path="/about.html" exact component={Error404} />
+        <Route path="/contacts.html" exact component={Error404} />
+        <Route path="/catalog.html" exact component={Error404} />
+        <Route path="/items/:id.html" exact component={Error404} />
+        <Route path="/cart.html" exact component={Error404} />
+        <Route path="*" component={Error404} /> CartIcon
+      </Switch>
     </Router>
   );
 }
